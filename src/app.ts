a38d3f4e29260ef64635e39d7dc5b3588cb07d0a
Mohammad Ismail Hossain <programmarsabbir@gmail.com>
2026-09-02 17:47:24 +0600

    create-two-database-table-successfully

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { type Request, type Response } from "express"
+import cookieParser from "cookie-parser"
 const app = express()
-const port = 5000
+app.use(cookieParser())
+app.use(express.json()); 
+app.use(express.text()); 
+app.use(express.urlencoded({ extended: true })); 
 
 app.get('/', (req:Request, res:Response) => {
  res.status(200).json({
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,36 @@
 import { Pool } from "pg";
 import config from "../config";
 export const pool = new Pool({
-connectionString : config.connnection_string
-})
+  connectionString: config.connnection_string,
+});
 
+export const initDB = async () => {
+  try {
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50),
+        email VARCHAR(50) UNIQUE NOT NULL , 
+        password TEXT NOT NULL ,
+        role VARCHAR(20)  DEFAULT 'contributor',
+        created_at TIMESTAMP DEFAULT NOW (),
+        updated_at TIMESTAMP DEFAULT NOW()
 
-export const initDB  =async ()=> {
-    console.log("database connectend successfully!")
-}
+        );
+        
+        CREATE TABLE IF NOT EXISTS issues (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(150) NOT NULL,
+        description TEXT NOT NULL,
+        type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+        status VARCHAR(20) DEFAULT 'open' CHECK (status IN ('open', 'in_progress', 'resolved')),
+        reporter_id INT REFERENCES users(id) ON DELETE CASCADE,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        );
+        `);
+    console.log("database connectend successfully!");
+  } catch (error) {
+    console.log("database connected failed", error);
+  }
+};
